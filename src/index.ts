181782ded660893e9cb05c6102f7169426223571
yash raj <yashraj0207@outlook.com>
2024-10-01 02:50:34 +0530

    initliaze repo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,74 @@
-import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger"
+import { Elysia, error, t } from "elysia"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+class Notes {
+  public data: string[] = []
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  constructor(data: string[] = ["Note 1", "Note 2", "Note 3"]) {
+    this.data = data
+  }
+
+  public add(note: string) {
+    this.data.push(note)
+  }
+
+  public edit(index: number, note: string) {
+    this.data[index] = note
+  }
+}
+
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .decorate("notes", new Notes())
+  .get("/notes", ({ notes }) => notes.data)
+  .get(
+    "/notes/:index",
+    ({ notes, params: { index } }) => {
+      const note = notes.data[index]
+      if (!note) {
+        return error(404, { message: "Note not found" })
+      }
+
+      return { data: note }
+    },
+    {
+      params: t.Object({
+        index: t.Number(),
+      }),
+    },
+  )
+  .post(
+    "/notes",
+    ({ notes, body }) => {
+      notes.add(body.note)
+      return notes.data
+    },
+    {
+      body: t.Object({
+        note: t.String(),
+      }),
+    },
+  )
+  .patch(
+    "/notes/:index",
+    ({ notes, params: { index }, body }) => {
+      if (!notes.data[index]) {
+        return error(404, { message: "Note not found" })
+      }
+
+      notes.edit(index, body.note)
+      return notes.data
+    },
+    {
+      params: t.Object({
+        index: t.Number(),
+      }),
+      body: t.Object({
+        note: t.String(),
+      }),
+    },
+  )
+  .listen(3000)
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
